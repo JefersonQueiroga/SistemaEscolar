2fdd7d8f726804eeda1744cf71a84f7d50a1ab7b
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2022-10-24 15:29:38 -0300

    Alterações do model do projeto.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from main.models import *
 # Register your models here.
+
+admin.site.register(Aluno)
+admin.site.register(Campus)
+admin.site.register(Disciplina)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
+from pyexpat import model
+from secrets import choice
 from django.db import models
 
-# Create your models here.
+CLASSES_CHOICES = [
+        ('TÉCNICO EM INFORMÁTICA', 'TÉCNICO EM INFORMÁTICA'),
+        ('TÉCNICO EM ALIMENTOS', 'TÉCNICO EM ALIMENTOS'),
+        ('TÉCNICO EM APICULTURA', 'TÉCNICO EM APICULTURA'),
+        ('QUÍMICA', 'QUÍMICA'),
+        ('ADS', 'ADS'),
+    ]
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=150)
+    carga_horaria = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Campus(models.Model):
+    nome = models.CharField(max_length=150)
+    nome_cidade = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=200)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    disciplinas = models.ManyToManyField(Disciplina)
+    curso = models.CharField(max_length=150,choices=CLASSES_CHOICES)
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
